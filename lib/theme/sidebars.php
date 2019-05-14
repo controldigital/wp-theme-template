95be177c2667ea6a9bdc813bf65f5a508a598cdb
controldigital <info@controldigital.nl>
2019-05-14 12:33:10 +0200

    Created default navigation sidebar

--- lib/theme/sidebars.php
+++ lib/theme/sidebars.php
@@ -18,6 +18,18 @@
 add_action( 'widgets_init', 'theme_sidebars' );
 function theme_sidebars() {
 
+	$args = array(
+		'id'            => 'sidebar-nav',
+		'class'         => 'nav',
+		'name'          => __( 'Navigation Sidebar', THEME_TEXT_DOMAIN ),
+		'description'   => __( 'Widget area after the main navigation', THEME_TEXT_DOMAIN ),
+		'before_title'  => '',
+		'after_title'   => '',
+		'before_widget' => '<li id="%1$s>',
+		'after_widget'  => '</li>',
+	);
+	register_sidebar( $args );
+
 	// $args = array(
 	// 	'id'            => '',
 	// 	'class'         => '',
