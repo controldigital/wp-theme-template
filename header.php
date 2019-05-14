55a5e334f27ecc4715b5f4564ad51927f3ec6793
controldigital <info@controldigital.nl>
2019-05-14 11:30:44 +0200

    Cleaned up wp_head and wp_body_open output.

--- header.php
+++ header.php
@@ -9,17 +9,20 @@
 <!-- Made by Control || controldigital.nl -->
 <html lang="<?php bloginfo( 'language' ); ?>" class="no-js">
 	<head>
-		<?php wp_head(); ?>
-		<?php get_template_part( './inc/cookies/cookies', 'head' ); ?>
+		
+		<?php 
+		/**
+		 * Head templates and scripts.
+		 * Modify scripts in ./inc/head.php.  
+		 */
+		wp_head(); ?>
+
 	</head>
 	<body <?php body_class(); ?>>
-		<?php get_template_part( './inc/cookies/cookies', 'body' ); ?>
-
-		<!-- Splash -->
-		<?php get_template_part( './inc/loader/splash' ); ?>
-
-		<!-- Navigation Default -->
-		<?php get_template_part( './inc/navigation/navigation', 'default' ); ?>
-
-		<!-- Hero Default -->
-		<?php get_template_part( './inc/hero/hero', 'default' ); ?>
+		
+		<?php
+		/**
+		 * Body open templates and scripts.
+		 * Modify scripts in ./inc/body.php.  
+		 */
+		wp_body_open(); ?>
