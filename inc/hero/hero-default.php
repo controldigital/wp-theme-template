8361092ff0b376b98b1906e3cc8f55a6f092ff54
controldigital <info@controldigital.nl>
2019-05-14 21:34:46 +0200

    Create default hero template

--- inc/hero/hero-default.php
+++ inc/hero/hero-default.php
@@ -6,3 +6,15 @@
  */
 
 ?>
+
+<header class="hero hero--default">
+	
+	<?php if ( has_post_thumbnail() ) { ?>
+		<div class="hero__thumbnail" style="background: url(<?php the_post_thumbnail_url(); ?>)"></div>
+	<?php } ?>
+	
+	<div class="hero__container">
+		<h1><?php the_title(); ?></h1>
+	</div>
+	
+</header>
