384067d7f768c5386f96efdb4c2b6481ce3d83f9
controldigital <info@controldigital.nl>
2019-05-14 11:30:08 +0200

    Created hooks file for output in wp_footer()

---
+++ lib/theme/footer.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Theme:			    
+ * Template:			footer.php
+ * Description:			Scripts and tags for in the footer
+ */
+
+
+/**
+ * footer_cookie_notice
+ * 
+ * Outputs the cookie notice at the bottom
+ * of the page.
+ * 
+ * @since   1.0
+ * @link    https://developer.wordpress.org/reference/functions/wp_footer/
+ */
+add_action( 'wp_footer', 'footer_cookie_notice', 10, 0 );
+function footer_cookie_notice() {
+    get_template_part( './inc/cookies/cookies', 'notice' );
+}
+
+/**
+ * footer_theme_templates
+ * 
+ * Add templates that have to be included
+ * at the bottom of the page.
+ * 
+ * @since   1.0
+ * @link    https://developer.wordpress.org/reference/functions/wp_footer/
+ */
+add_action( 'wp_footer', 'footer_theme_templates', 11, 0 );
+function footer_theme_templates() {
+    
+    // JS polyfill scanner
+	get_template_part( './inc/footer/footer', 'polyfill' );
+}
