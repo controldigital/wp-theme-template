905ac6d78b32a6ad32a87e5219eb421cc96595e0
controldigital <info@controldigital.nl>
2019-05-15 14:30:42 +0200

    Removed post loop

--- inc/content/content-404.php
+++ inc/content/content-404.php
@@ -9,8 +9,4 @@ $title 		= get_option( 'theme-404-title' );
 $content 	= get_option( 'theme-404-content' );
 ?>
 
-<?php if ( have_posts() ) { while ( have_posts() ) { the_post(); ?>
-
-    <?php echo $content; ?>
-
-<?php } } ?>
+<?php echo $content; ?>
