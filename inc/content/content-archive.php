4a89e1f39b59bb7274cad3c6f663d32c23aa667f
controldigital <info@controldigital.nl>
2019-05-15 14:30:52 +0200

    Echo title and content fields

--- inc/content/content-archive.php
+++ inc/content/content-archive.php
@@ -10,8 +10,11 @@ $title 		= get_option( 'theme-archives-title' );
 $content 	= get_option( 'theme-archives-content' );
 ?>
 
+<?php echo $title; ?>
+<?php echo $content; ?>
+
 <?php if ( have_posts() ) { while ( have_posts() ) { the_post(); ?>
-
-    <?php echo $content; ?>
-
+    
+    <?php the_content(); ?>
+    
 <?php } } ?>
