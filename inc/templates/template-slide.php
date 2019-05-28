49a016ff296231b45397740f003a69041c25f762
EmielZuurbier <emielzuurbier@outlook.com>
2019-05-28 20:57:50 +0200

    Added template slide inner and CSS

--- inc/templates/template-slide.php
+++ inc/templates/template-slide.php
@@ -8,7 +8,58 @@
 ?>
 
 <template id="template-slide">
+
+	<style>
+
+		*, 
+		*::before, 
+		*::after {
+			box-sizing: border-box;
+			margin: 0;
+			padding: 0;
+		}
+
+		:host {
+			display: block;
+			position: relative;
+			font-size: 100%;
+			height: 100%;
+		}
+
+		.wrapper {
+			display: grid;
+			grid-template-rows: 1fr;
+			grid-template-columns: 1fr;
+			grid-template-areas: "wrapper";
+			height: 100%;
+		}
+
+		.thumbnail,
+		.inner {
+			grid-area: wrapper;
+		}
+
+		.inner {
+			padding: 1em;
+		}
+		
+	</style>
+
 	<article class="wrapper">
 		
+		<div class="thumbnail">
+			<slot name="thumbnail"></slot>
+		</div>
+
+		<div class="inner">
+			<header class="header">
+				<slot name="title"></slot>
+			</header>
+
+			<div class="body">
+				<slot name="body"></slot>
+			</div>
+		</div>
+
 	</article>
 </template>
