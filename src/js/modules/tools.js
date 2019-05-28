4015f28cac416acf5fb7555e6d97c3ba38390893
EmielZuurbier <emielzuurbier@outlook.com>
2019-05-28 21:04:28 +0200

    Added createDirections function

--- src/js/modules/tools.js
+++ src/js/modules/tools.js
@@ -271,3 +271,29 @@ export const lazyLoadImages = (images = document.images) =>
 	[...images]
 		.filter(imageIsLazyLoadable)
 		.map((image) => lazyLoadImage(image));
+
+/**
+ * Creates a new object with a horizontal and
+ * a vertical value
+ * 
+ * @function	createDirections
+ * @param 		{*} horizontal The horzontal value.
+ * @param		{*} vertical The vertical value. 
+ * @param		{Function} callback Function to run over the horizontal and vertical value. Must return a value.
+ * @returns		{(Object|Boolean)}
+ */
+export const createDirections = (horizontal, vertical, callback) => {
+	if (horizontal === undefined || vertical === undefined) {
+		return null;
+	}
+	if (callback && 'function' === typeof callback) {
+		return {
+			horizontal: callback(horizontal),
+			vertical: callback(vertical)
+		};
+	}
+	return {
+		horizontal,
+		vertical
+	};
+}
