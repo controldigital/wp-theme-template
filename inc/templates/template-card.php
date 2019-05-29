9e37ebf4cc65a0f28ec42969b3e68f43ce5674de
controldigital <info@controldigital.nl>
2019-05-29 11:14:13 +0200

    Added card template and Shadow CSS

--- inc/templates/template-card.php
+++ inc/templates/template-card.php
@@ -9,4 +9,50 @@
 
 <template id="template-card">
 
+    <style>
+
+        *, 
+		*::before, 
+		*::after {
+			box-sizing: border-box;
+			margin: 0;
+			padding: 0;
+		}
+
+        :host {
+            display: block;
+            width: 100%;
+            height: 100%;
+        }
+
+        .container {
+            display: flex;
+            flex-flow: column-wrap;
+        }
+
+        .thumbnail {
+            flex: 0 1 auto;
+        }
+
+        .body {
+            flex: 1 1 100%;
+        }
+
+    </style>
+
+    <article class="container">
+
+        <header class="header">
+            <figure class="thumbnail">
+                <slot name="thumbnail"></slot>
+            </figure>
+            <slot name="title">
+        </header>
+
+        <div class="body">
+            <slot name="body"></slot>
+        </div>
+
+    </article>
+
 </template>
