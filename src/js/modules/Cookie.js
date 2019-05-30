7a4f3962403f9912bb5ce7fec80c4ab6f96ab925
EmielZuurbier <emielzuurbier@outlook.com>
2019-05-31 01:11:58 +0200

    Added methods to work with the global cookie var

--- src/js/modules/Cookie.js
+++ src/js/modules/Cookie.js
@@ -11,36 +11,11 @@
 export default class Cookie {
 
     /**
-     * Creates cookie with a name, value, expire date, path and a domain.
-     * Returns the cookie of the document.
-     *
-     * @static
-     * @method  	set
-     * @since 		1.0
-     * 
-     * @param 		{String} name Name of cookie
-     * @param 		{String} value Value of cookie
-     * @param 		{Number} expire When cookie expires in days
-     * @param 		{String} path Path to store cookie
-     * @param   	{String} [domain] The domain to store the cookie
-     * @returns 	{String} Returns the cookie string
-     */
-    static set(name, value, expire, path = '/', domain = location.hostname.replace(/^www\./i, "")) {
-        const date = new Date();
-        date.setTime(date.getTime() + (expire * 24 * 3600 * 1000));
-        const expires = date.toUTCString();
-        document.cookie = name + '=' + value + '; expires=' + expires + '; path=' + path + '; domain=' + domain;
-        return document.cookie;
-    }
-
-    /**
      * Retrieves a cookie from the document.
      * Returns a string if the cookie is found or false when it is not.
      *
      * @static
      * @method  	get
-     * @since 		1.0
-     * 
      * @param 		{String} name Cookie to fetch
      * @returns 	{(String|Boolean)} Returns cookie on success, false on fail
      */
@@ -58,6 +33,27 @@ export default class Cookie {
     }
 
     /**
+     * Creates cookie with a name, value, expire date, path and a domain.
+     * Returns the cookie of the document.
+     *
+     * @static
+     * @method  	set
+     * @param 		{String} name Name of cookie
+     * @param 		{String} value Value of cookie
+     * @param 		{Number} expire When cookie expires in days
+     * @param 		{String} path Path to store cookie
+     * @param   	{String} [domain] The domain to store the cookie
+     * @returns 	{String} Returns the cookie string
+     */
+    static set(name, value, expire, path = '/', domain = location.hostname.replace(/^www\./i, "")) {
+        const date = new Date();
+        date.setTime(date.getTime() + (expire * 24 * 3600 * 1000));
+        const expires = date.toUTCString();
+        document.cookie = name + '=' + value + '; expires=' + expires + '; path=' + path + '; domain=' + domain;
+        return document.cookie;
+    }
+
+    /**
      * Checks if the cookie exists in thedocument.cookie string. 
      * Returns a boolean.
      * 
@@ -67,11 +63,10 @@ export default class Cookie {
      * @returns     {Boolean}
      */
     static has(name) {
-        const nameExpression = `${name}=`;
         const cookies = document.cookie.split(';');
         for (let i = 0; i < cookies.length; i += 1) {
             let currentCookie = cookies[i].trim();
-            if (currentCookie.indexOf(nameExpression) === 0) {
+            if (currentCookie.indexOf(`${name}=`) === 0) {
                 return true;
             }
         }
@@ -83,8 +78,6 @@ export default class Cookie {
      *
      * @static
      * @method      delete
-     * @since 	    1.0
-     * 
      * @param 	    {String} name Cookie to delete
      * @returns     {String} Returns the cookie
      */
@@ -92,5 +85,63 @@ export default class Cookie {
         document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
         return document.cookie;
     };
+
+    /**
+	 * Returns the cookie name set in WP Customizer
+	 * or else return a empty string.
+	 * 
+     * @static
+	 * @method      getName
+	 * @since	    1.0
+	 * @returns	    {String}
+	 */
+	static getName() {
+		return cookie.name ? cookie.name : '';
+    };
+    
+    /**
+	 * Creates a cookie friendly URL for 
+	 * the domain parameter
+	 * 
+     * @static
+	 * @method      getDomain
+	 * @since	    1.0
+	 * @returns	    {String}
+	 */
+	static getDomain() {
+		const url = window.location.host + window.location.pathname;
+		const domain = url.replace("//", "/");
+		return domain;
+    };
+    
+    /**
+	 * Returns the amount of days for the
+	 * cookie to expire in a floating point
+	 * format or default year in days.
+	 * 
+     * @static
+	 * @method      getExpirationDate
+	 * @since	    1.0
+	 * @returns	    {Number}
+	 */
+	static getExpirationDate() {
+		return cookie.expire ? parseFloat(cookie.expire) : 365;
+    };
+    
+    /**
+	 * Returns the scripts for in the head
+	 * as set in the WP Customizer.
+	 * 
+     * @static
+	 * @method      getScripts
+	 * @since	    1.0
+	 * @returns	    {Array}
+	 */
+	static getScripts() {
+        return {
+            head: cookie.head ? cookie.head : '',
+            body: cookie.body ? cookie.body : ''
+        };
+    }
 
 }
