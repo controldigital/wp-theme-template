e08e9d3bd06b622776c047426db44273f71a53ed
EmielZuurbier <emielzuurbier@outlook.com>
2019-06-05 16:13:23 +0200

    Template for cookie with a default layout

--- inc/templates/template-cookie.php
+++ inc/templates/template-cookie.php
@@ -1,15 +1,12 @@
 <?php
 /**
  * Theme:
- * Template:			cookies-notice.php
- * Description:			Cookies notice to give user controls over cookies
+ * Template:			template-cookie.php
+ * Description:			
  */
 
 // Get URL of current page
 global $wp;
-
-// Cookie active?
-$cookie_active = get_theme_mod( 'cookie_active' );
 
 // Name of cookie variable
 $cookie_name = get_theme_mod( 'cookie_name' );
@@ -17,10 +14,63 @@ $cookie_expiration_date = get_theme_mod( 'cookie_expiration_date' );
 ?>
 
 <template id="template-cookie">
+
+	<style>
+
+		*, 
+		*::before, 
+		*::after {
+			box-sizing: border-box;
+			margin: 0;
+			padding: 0;
+		}
+
+		:host {
+			all: initial;
+			display: flex;
+			align-items: flex-end;
+			justify-content: center;
+			contain: content;
+			position: relative;
+			font-size: 100%;
+			position: fixed;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+		}
+
+		.wrapper {
+			width: 100%;
+			max-width: 20rem;
+			height: auto;
+		}
+
+		.form {
+			display: grid;
+			grid-template-rows: auto auto auto;
+			grid-template-columns: 1fr;
+			grid-template-areas: 
+				"header"
+				"body"
+				"footer";
+		}
+
+		.header {
+			grid-area: header;
+		}
+
+		.body {
+			grid-area: body;
+		}
+
+		.footer {
+			grid-area: footer;
+		}
+
+	</style>
 	
 	<article class="wrapper">
-
-		<slot name="thumbnail"></slot>
 
 		<form class="form" method="POST" action="<?php echo admin_url( 'admin-post.php' ); ?>">
 
@@ -31,11 +81,16 @@ $cookie_expiration_date = get_theme_mod( 'cookie_expiration_date' );
 			<input type="hidden" name="_wp_referrer" value="<?php echo home_url( $wp->request ); ?>">
 
 			<header class="header">
-				<slot name="title"></slot>
+				<slot name="thumbnail"></slot>
+				<slot name="title">
+					<h3><?php _e( 'Cookie consent', THEME_TEXT_DOMAIN ); ?></h3>
+				</slot>
 			</header>
 
 			<div class="body">
-				<slot name="content"></slot>
+				<slot name="content">
+					<p><?php _e( 'This site uses cookies to ensure that it works properly.', THEME_TEXT_DOMAIN ); ?></p>
+				</slot>
 			</div>
 
 			<footer class="footer">
