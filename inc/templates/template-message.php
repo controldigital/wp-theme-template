360d2fd8918f1436498f71236215bae7dcdfc80d
EmielZuurbier <emielzuurbier@outlook.com>
2019-06-09 01:01:41 +0200

    Create template message template

---
+++ inc/templates/template-message.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Theme:
+ * Template:			template-message.php
+ * Description:	
+ */
+
+?>
+
+<template id="template-message">
+
+	<style>
+
+		*, 	
+		*::before, 
+		*::after {
+			box-sizing: border-box;
+			margin: 0;
+			padding: 0;
+		}
+
+		:host {
+			all: initial;
+			display: block;
+			contain: content;
+		}
+
+		:host([position="top"]) {
+
+		}
+
+		:host([position="right"]) {
+			
+		}
+
+		:host([position="bottom"]) {
+			
+		}
+
+		:host([position="left"]) {
+			
+		}
+
+		:host([type="warning"]) {
+			
+		}
+
+		:host([type="info"]) {
+			
+		}
+
+		:host([type="error"]) {
+			
+		}
+
+	</style>
+
+</template>
