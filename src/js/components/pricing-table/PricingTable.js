6d0df06f5a8d9ff5e1ca4d13a08ca46bdd9c3d6e
EmielZuurbier <emielzuurbier@outlook.com>
2019-06-09 01:06:39 +0200

    Add attachShadowToElement function

---
+++ src/js/components/pricing-table/PricingTable.js
@@ -0,0 +1,91 @@
+/**
+ * @module		./components/service/Service
+ */
+
+import { attachShadowToElement } from '../shadow.js';
+
+// ID of HTML template for Shadow DOM.
+const templateId = 'template-pricing-table';
+
+/**
+ * Element to display a pricing table.
+ * 
+ * @class
+ * @extends	HTMLElement
+ */
+export default class HTMLPricingTableElement extends HTMLElement {
+
+	/**
+	 * Attributes to trigger the attributeChangedCallback on.
+	 * 
+	 * @static
+	 * @get
+	 * @method	observedAttributes
+	 * @returns	{String[]}
+	 */
+	static get observedAttributes() {
+		return ['featured'];
+	}
+
+	/**
+	 * @constructor
+	 */
+	constructor() {
+		super();
+
+		// Create a Shadow DOM.
+		attachShadowToElement.call(this, templateId);
+		
+	}
+
+	/**
+	 * Fires when an attribute has been changed.
+	 * 
+	 * @method	attributeChangedCallback
+	 * @param 	{String} attrName Name of attribute.
+	 * @param 	{*} oldValue Old value of attribute.
+	 * @param 	{*} newValue New value of attribute.
+	 */
+	attributeChangedCallback(attrName, oldValue, newValue) {
+		
+		if (attrName === 'featured') {
+			if (newValue !== null) {
+
+				
+
+			}
+		}
+
+	}
+
+	/**
+	 * Fires when the element has been connected.
+	 * 
+	 * @method	connectedCallback
+	 * @returns	{void}
+	 */
+	connectedCallback() {
+
+	}
+
+	/**
+	 * Fires when the element has been disconnected.
+	 * 
+	 * @method	disconnectedCallback
+	 * @returns	{void}
+	 */
+	disconnectedCallback() {
+
+	}
+
+	/**
+	 * Fires when the element has been adopted on a new page.
+	 * 
+	 * @method	connectedCallback
+	 * @returns	{void}
+	 */
+	adoptedCallback() {
+
+	}
+
+}
