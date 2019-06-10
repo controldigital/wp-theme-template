910d4c9513ee7830dc89bf1b067b3e84097e0c2a
EmielZuurbier <emielzuurbier@outlook.com>
2019-06-10 20:44:05 +0200

    Create svg icon containers

---
+++ src/js/components/like/svg.js
@@ -0,0 +1,24 @@
+/**
+ * @module		./components/like/svg
+ */
+
+/**
+ * TODO: Get some svg icons for the thumb, heart and star
+ */
+export const renderThumbSVG = () => `
+	<svg viewbox="0 0 25 25">
+
+	</svg>
+`;
+
+export const renderHeartSVG = () => `
+	<svg viewbox="0 0 25 25">
+
+	</svg>
+`;
+
+export const renderStarSVG = () => `
+	<svg viewbox="0 0 25 25">
+
+	</svg>
+`;
