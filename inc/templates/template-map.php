ae0bc48ed9cf746dd39a3c42ddd412afdb275bbe
EmielZuurbier <emielzuurbier@outlook.com>
2019-06-17 08:51:19 +0200

    Create a map template

---
+++ inc/templates/template-map.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Theme:				
+ * Template:			template-map.php
+ * Description:			
+ */
+
+?>
+
+<template id="template-map">
+
+    <style>
+
+        *, 
+		*::before, 
+		*::after {
+			box-sizing: border-box;
+			margin: 0;
+			padding: 0;
+		}
+
+        :host {
+            all: initial;
+			display: block;
+			contain: content;
+        }
+
+    </style>
+
+	<div class="map"></div>
+
+</template>
