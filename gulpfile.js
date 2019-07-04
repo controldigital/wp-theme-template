d3f6819a57552dd9963671dae71857b21d0a493a
EmielZuurbier <emielzuurbier@outlook.com>
2019-07-04 23:26:58 +0200

    Add critical CSS to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const webpack = require('webpack');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const criticalCSS = require('gulp-penthouse');
 
 /**
  * Styles gulp task
@@ -13,7 +14,7 @@ const autoprefixer = require('gulp-autoprefixer');
  * {@link https://github.com/postcss/autoprefixer#options}
  * 
  */
-gulp.task('styles', function() {
+gulp.task('styles', () => {
     return gulp.src('./src/scss/style.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(autoprefixer({
@@ -33,14 +34,66 @@ gulp.task('styles', function() {
  * {@link https://webpack.js.org/configuration/}
  * 
  */
-gulp.task('scripts', function(callback) {
+gulp.task('scripts', callback => {
     webpack(require('./webpack.config.js'), function(err, stats) {
-      if (err) {
-        console.log(err.toString());
-      }
-      console.log(stats.toString());
-      callback();
+    	if (err) {
+        	console.log(err.toString());
+    	}
+      	console.log(stats.toString());
+      	callback();
     });
+});
+
+/**
+ * Critical CSS gulp task
+ * 
+ * Creates a critical CSS file.
+ * The source of the file can be changed if needed
+ * 
+ * How it works
+ * 
+ * For options check out:
+ * {@link https://www.npmjs.com/package/penthouse}
+ * 
+ */
+gulp.task('critical', () => {
+
+	/**
+	 * List of URLs to check and create critical CSS files for.
+	 * Add your own URL and filename to the array to create
+	 * multiple critical CSS files
+	 * 
+	 * @type		{Object[]} An array of objects with an url and out parameter
+	 * @property	{String} object.url The URL to get the critical styles from
+	 * @property	{String} object.fileName The name and type of the output file
+	 */
+	const criticalPaths = [
+		{
+			url: '', // url from where we want penthouse to extract critical styles
+			fileName: '', // output file name
+		},
+	];
+	
+	return criticalPaths.forEach((path) => {
+		gulp.src('./dist/css/style.css')
+
+			/**
+			 * @property	{String} url The URL to extract the critical CSS from
+			 * @property	{String} out The file that the function outputs
+			 * @property	{Number} width Maximum width of page to check
+			 * @property	{Number} height Maximum height of page to check
+			 * @property	{String} userAgent Useragent to check the page with
+			 */
+			.pipe(criticalCSS({
+				url: path.url,
+				out: path.fileName,
+				width: 1400,
+				height: 900,
+				userAgent: 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)' // pretend to be googlebot when grabbing critical page styles.
+			}))
+			.pipe(cleanCSS({compatibility: '*'}))
+			.pipe(gulp.dest('./dist/critical/'));
+	});
 });
 
 /**
