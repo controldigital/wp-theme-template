805434f6238fbc29d9ac58d3a4b321aa64a4c793
EmielZuurbier <emielzuurbier@outlook.com>
2019-08-04 12:47:48 +0200

    Add documentation

--- node_scripts/sftp-config/build.js
+++ node_scripts/sftp-config/build.js
@@ -1,6 +1,29 @@
+/**
+ * Run a configuration for building the sftp.json needed for the SFTP Vscode plugin.
+ * This will trigger a series of questions to complete your sftp.json for this project.
+ * 
+ * @author	Emiel Zuurbier
+ * @package	build.js
+ * @example 
+ * npm run sftp
+ */
+
 const fs = require('fs').promises;
 const prompts = require('prompts');
 const config = require('./config.js');
+
+/**
+ * Logs a error message to the console.
+ * 
+ * @function	logError
+ * @param 		{string} error Error messsage.
+ * @returns		{void}
+ */
+const logError = (error) => {
+	if (error) {
+		console.error(error);
+	}
+}
 
 /**
  * Prompts questions for the user to fill in.
@@ -10,7 +33,9 @@ const config = require('./config.js');
  * @returns		{Promise<Object>}
  */
 const askInformation = async () => {
+
 	try {
+		// Get the responses from the prompts.
 		const responses = await prompts([
 			{
 				type: 'text',
@@ -52,9 +77,13 @@ const askInformation = async () => {
 	} catch(error) {
 		console.log(error);
 	}
+
 }
 
 /**
+ * Combine the responses with the default config setup.
+ * Adds file ignores and watchers for common files.
+ * 
  * @function	createContentForFile
  * @param 		{Object} responses 
  * @returns		{string}
@@ -65,43 +94,46 @@ const createContentForFile = (responses) => {
 };
 
 /**
+ * Creates a directory in the theme folder named .vscode.
+ * The content given will be put into a file called sftp.json and placed into the .vscode folder.
+ * 
  * @function	createSFTPConfigFile
  * @param 		{string} content 
  * @returns		{Promise}
  */
 const createSFTPConfigFile = async (content) => {
 
-	const onError = (error) => {
-		if (error) {
-			throw error;
-		}
-	}
-
 	try {
+		// Create a new directory named .vscode.
 		await fs.mkdir('.vscode', { recursive: true });
 		console.log('.vscode folder created');
 	} catch (error) {
-		onError(error)
+		logError(error)
 	}
 
 	try {
+		// Write a file to the .vscode directory with the content.
 		await fs.writeFile('.vscode/sftp.json', content);
 		console.log('sftp.json file has been generated');
 	} catch (error) {
-		onError(error)
+		logError(error)
 	}
 
 };
 
 /**
+ * Ask for the queries and use their responses to create a new
+ * sftp.json file that will be placed into the .vscode folder.
+ * 
  * @function		build
  * @returns			{void}
  */
 const build = async () => {
 	const responses = await askInformation();
 	const content = createContentForFile(responses);
-	console.log(content);
+	console.log('SFTP content generated: ', content);
 	createSFTPConfigFile(content);
 }
 
+// Start building.
 build();
