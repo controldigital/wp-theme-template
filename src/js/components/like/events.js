88a16b3d671a98cc61165d9d355b0cb612a591b9
EmielZuurbier <emielzuurbier@outlook.com>
2019-08-11 20:48:12 +0200

    Change onChange into onClick.

--- src/js/components/like/events.js
+++ src/js/components/like/events.js
@@ -2,16 +2,22 @@
  * @module		./components/like/events
  */
 
+import { 
+	addIdToLocalStorage,
+	removeIdFromLocalStorage
+} from './storage.js';
 
 /**
- * @function	onChange
+ * @function	onClick
  * @param 		{Event} event 
  * @returns		{void}
  */
-export const onChange = function onChange({ target }) {
-	if (target.checked === true) {
+export const onClick = function onClick({ target }) {
+	if (this.clicked === null) {
 		this.clicked = true;
+		addIdToLocalStorage(this.name, this.value);
 	} else {
 		this.clicked = false;
+		removeIdFromLocalStorage(this.name, this.value);
 	}
 };
