05e9dc9237597541705b4609a58b5df8017a46ac
EmielZuurbier <emielzuurbier@outlook.com>
2019-08-11 20:47:12 +0200

    Select SVG through a single function

--- src/js/components/like/svg.js
+++ src/js/components/like/svg.js
@@ -3,22 +3,31 @@
  */
 
 /**
- * TODO: Get some svg icons for the thumb, heart and star
+ * Returns a string to render an icon with.
+ * 
+ * @function	renderSVGIcon
+ * @param 		{String} icon Icon to render
+ * @returns		{String} The icon in string or an empty string if the icon arguments does not match any.
  */
-export const renderThumbSVG = () => `
-	<svg viewbox="0 0 25 25">
+export const renderSVGIcon = (icon) => {
+	if (icon === 'thumb') {
+		return /*template*/`
+			<svg class="thumb" viewbox="0 0 25 25">
+				
+			</svg>
+		`;
+	} else if (icon === 'heart') {
+		return /*template*/`
+			<svg class="heart" viewbox="0 0 25 25">
 
-	</svg>
-`;
-
-export const renderHeartSVG = () => `
-	<svg viewbox="0 0 25 25">
-
-	</svg>
-`;
-
-export const renderStarSVG = () => `
-	<svg viewbox="0 0 25 25">
-
-	</svg>
-`;
+			</svg>
+		`;
+	} else if (icon === 'star') {
+		return /*template*/`
+			<svg class="star" viewbox="0 0 25 25">
+				
+			</svg>
+		`;
+	} 
+	return '';
+};
