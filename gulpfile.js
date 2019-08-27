ec9509c5d3cd8ab4d0588978324d51f0d8841d2f
jeroenrodenburg <jeroen@rodenburg.io>
2019-08-27 15:35:27 +0200

    Move browserslist from gulpfile to package.json

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,6 @@ gulp.task('styles', function() {
     return gulp.src('./src/scss/style.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
         cascade: false,
         grid: true
     }))
