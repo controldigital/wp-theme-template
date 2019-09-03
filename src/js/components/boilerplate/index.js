658fd663e6bc4cbe67b2e8eb3d5bdecba243bbc2
controldigital <info@controldigital.nl>
2019-09-03 14:59:29 +0200

    Component boilerplate

---
+++ src/js/components/boilerplate/index.js
@@ -0,0 +1,73 @@
+/**
+ * @module		./components/../..
+ */
+
+/**
+ * 
+ * @class
+ * @extends	HTMLElement
+ */
+export default class CustomElement extends HTMLElement {
+
+	/**
+	 * Attributes to trigger the attributeChangedCallback on.
+	 * 
+	 * @static
+	 * @get
+	 * @method	observedAttributes
+	 * @returns	{String[]}
+	 */
+	static get observedAttributes() {
+		return [];
+	}
+
+	/**
+	 * @constructor
+	 */
+	constructor() {
+		super();
+	}
+
+	/**
+	 * Fires when an attribute has been changed.
+	 * 
+	 * @method	attributeChangedCallback
+	 * @param 	{String} attrName Name of attribute.
+	 * @param 	{*} oldValue Old value of attribute.
+	 * @param 	{*} newValue New value of attribute.
+	 */
+	attributeChangedCallback(attrName, oldValue, newValue) {
+
+	}
+
+	/**
+	 * Fires when the element has been connected.
+	 * 
+	 * @method	connectedCallback
+	 * @returns	{void}
+	 */
+	connectedCallback() {
+
+	}
+
+	/**
+	 * Fires when the element has been disconnected.
+	 * 
+	 * @method	disconnectedCallback
+	 * @returns	{void}
+	 */
+	disconnectedCallback() {
+
+	}
+
+	/**
+	 * Fires when the element has been adopted in a new document.
+	 * 
+	 * @method	connectedCallback
+	 * @returns	{void}
+	 */
+	adoptedCallback() {
+
+	}
+
+}
