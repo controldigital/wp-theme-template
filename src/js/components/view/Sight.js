ed63691e50bbe2b5a5187efe1eded5ca900d978b
controldigital <info@controldigital.nl>
2019-09-06 17:12:10 +0200

    Add sight component

---
+++ src/js/components/view/Sight.js
@@ -0,0 +1,94 @@
+/**
+ * @module		./components/view/Sight
+ */
+
+/**
+ * 
+ * @class
+ * @extends	HTMLElement
+ */
+export default class HTMLSightElement extends HTMLElement {
+
+	/**
+	 * Attributes to trigger the attributeChangedCallback on.
+	 * 
+	 * @static
+	 * @get
+	 * @method	observedAttributes
+	 * @returns	{String[]}
+	 */
+	static get observedAttributes() {
+		return ['visible'];
+	}
+
+	/**
+	 * @constructor
+	 */
+	constructor() {
+		super();
+	}
+
+	/**
+	 * Gets and sets the visible attribute.
+	 * @property
+	 */
+	get visible() {
+		return this.getAttribute('visible');
+	}
+
+	set visible(value) {
+		if (value === true) {
+			this.setAttribute('visible', '');
+		} else {
+			this.removeAttribute('visible');
+		}
+	}
+
+	/**
+	 * Fires when an attribute has been changed.
+	 * 
+	 * @method	attributeChangedCallback
+	 * @param 	{String} attrName Name of attribute.
+	 * @param 	{*} oldValue Old value of attribute.
+	 * @param 	{*} newValue New value of attribute.
+	 */
+	attributeChangedCallback(attrName, oldValue, newValue) {
+
+		switch(attrName) {
+			case 'visible':
+				break;
+		}
+
+	}
+
+	/**
+	 * Fires when the element has been connected.
+	 * 
+	 * @method	connectedCallback
+	 * @returns	{void}
+	 */
+	connectedCallback() {
+
+	}
+
+	/**
+	 * Fires when the element has been disconnected.
+	 * 
+	 * @method	disconnectedCallback
+	 * @returns	{void}
+	 */
+	disconnectedCallback() {
+
+	}
+
+	/**
+	 * Fires when the element has been adopted in a new document.
+	 * 
+	 * @method	connectedCallback
+	 * @returns	{void}
+	 */
+	adoptedCallback() {
+
+	}
+
+}
