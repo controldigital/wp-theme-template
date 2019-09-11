01bb8156421527e978457a7396e9f99124f9d39f
EmielZuurbier <emielzuurbier@outlook.com>
2019-09-11 21:49:27 +0200

    Add typedef

--- src/js/components/slider/functions.js
+++ src/js/components/slider/functions.js
@@ -15,6 +15,13 @@ export const getRailsOffset = function getRailsOffset() {
 	};
 };
 
+/**
+ * @typedef		directionsObject
+ * @type 		{Object} obj
+ * @param		{string} horizontal  
+ * @param		{string} vertical
+ */
+
  /**
  * Creates a new object with a horizontal and a vertical value
  * 
@@ -22,7 +29,7 @@ export const getRailsOffset = function getRailsOffset() {
  * @param 		{*} horizontal The horzontal value.
  * @param		{*} vertical The vertical value. 
  * @param		{Function} callback Function to run over the horizontal and vertical value. Must return a value.
- * @returns		{(Object|null)}
+ * @returns		{(directionsObject|null)}
  */
 export const createDirections = (horizontal, vertical, callback) => {
 	if (horizontal === undefined || vertical === undefined) {
