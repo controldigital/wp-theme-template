67f42e726141c73fa0703340ea37560f1b9a5a61
jeroenrodenburg <jeroen@rodenburg.io>
2019-09-17 15:19:49 +0200

    Improve sidebar widget enclosing tags

--- inc/footer/footer-default.php
+++ inc/footer/footer-default.php
@@ -11,10 +11,26 @@
 	<div class="footer__container">
 		<div class="footer__row">
 
-			<?php dynamic_sidebar( 'sidebar-footer-1' ); ?>
-			<?php dynamic_sidebar( 'sidebar-footer-2' ); ?>
-			<?php dynamic_sidebar( 'sidebar-footer-3' ); ?>
-			<?php dynamic_sidebar( 'sidebar-footer-4' ); ?>
+			<?php if ( is_active_sidebar( 'sidebar-footer-1' ) ) { ?>
+				<div class="footer__column footer__column-1">
+					<?php dynamic_sidebar( 'sidebar-footer-1' ); ?>
+				</div>
+			<?php } ?>
+			<?php if ( is_active_sidebar( 'sidebar-footer-2' ) ) { ?>
+				<div class="footer__column footer__column-2">
+					<?php dynamic_sidebar( 'sidebar-footer-2' ); ?>
+				</div>
+			<?php } ?>
+			<?php if ( is_active_sidebar( 'sidebar-footer-3' ) ) { ?>
+				<div class="footer__column footer__column-3">
+					<?php dynamic_sidebar( 'sidebar-footer-3' ); ?>
+				</div>
+			<?php } ?>
+			<?php if ( is_active_sidebar( 'sidebar-footer-4' ) ) { ?>
+				<div class="footer__column footer__column-4">
+					<?php dynamic_sidebar( 'sidebar-footer-4' ); ?>
+				</div>
+			<?php } ?>
 
 		</div>
 	</div>
