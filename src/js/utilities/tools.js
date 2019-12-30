568c37016286cfc3f1b28a0c092f4a82919a8834
EmielZuurbier <emielzuurbier@outlook.com>
2019-12-30 14:39:43 +0100

    Add alphaNumericString and UUID generators

--- src/js/utilities/tools.js
+++ src/js/utilities/tools.js
@@ -230,6 +230,32 @@ export const stringCamelToSnake = string => string.replace(/[A-Z\s]+/g, match =>
 export const stringSnakeToCamel = string => string.replace(/_\w/g, match => match[1].toUpperCase());
 
 /**
+ * Generates a random alphanumeric string based on a given length and returns it.
+ * 
+ * @function	generateAlphaNumericString
+ * @param 		{number} [length=10] 
+ * @returns		{string}
+ */
+export const generateAlphaNumericString = (length = 10) => {
+	const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+	let value = '';
+	let i = 0;
+	let n = charset.length;
+	for (i; i < length; i++) {
+		value += charset.charAt(Math.floor(Math.random() * n));
+	}
+	return value;
+};
+
+/**
+ * Generates a unique UUID that --almost-- guarantees that the value is unique.
+ * 
+ * @function	uuid
+ * @returns		{string}
+ */
+export const uuid = () => ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
+
+/**
  * Converts an array into a comma seperated value (CSV) string.
  * 
  * @function	arrayToCSV
