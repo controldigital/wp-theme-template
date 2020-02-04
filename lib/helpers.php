ce5510c1e76616af5a6e18675de15ac600af34b0
jeroenrodenburg <jeroen@rodenburg.io>
2020-02-04 14:43:14 +0100

    update package.json, add get_hero function

--- lib/helpers.php
+++ lib/helpers.php
@@ -372,3 +372,32 @@ function the_breadcrumb( $show_on_home = false, $delimiter = '/', $home = 'Home'
 	}
 
 }
+
+/**
+ * Gets the hero template using get_template_part.
+ * 
+ * @param	string $name Specific template to get.
+ */
+function get_hero( $name = null ) {
+	get_template_part( './template-parts/hero/hero', $name );
+}
+
+/**
+ * Gets the layout template using get_template_part.
+ * 
+ * @param	string $name Specific template to get.
+ */
+function get_layout( $name = null ) {
+	get_template_part( './template-parts/layout/layout', $name );
+}
+
+/**
+ * TODO: Create a cool debug function
+ */
+function debug( $var ) {
+	echo '<pre>';
+	var_dump($var);
+	echo '</pre>';
+}
+
+
