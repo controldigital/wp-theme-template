b83d81c9bdf08fe231fdb561c165ea86c21f6b53
jeroenrodenburg <jeroen@rodenburg.io>
2020-04-06 17:42:47 +0200

    Update gulp/gulpfile to version 4

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 
 /**
- * Styles gulp task
+ * Styles gulp function
  * 
  * Compiles the SCSS files to CSS and minifies
  * and autoprefixes the CSS. 
@@ -13,19 +13,16 @@ const autoprefixer = require('gulp-autoprefixer');
  * {@link https://github.com/postcss/autoprefixer#options}
  * 
  */
-gulp.task('styles', () => 
-    gulp.src('./src/scss/style.scss')
-    .pipe(sass({
-        outputStyle: 'compressed'
-    }).on('error', sass.logError))
-    .pipe(autoprefixer({
-        cascade: false,
-    }))
+function styles(done) {
+    gulp.src('./src/scss/style.scss', { sourcemaps: true })
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer({cascade: false}))
     .pipe(gulp.dest('./dist/'))
-);
+    done();
+};
 
 /**
- * Scripts gulp task
+ * Scripts gulp function
  * 
  * Starts Webpack to bundle the JS Modules and turn
  * ES6 code into legacy code for older browsers.
@@ -34,54 +31,30 @@ gulp.task('styles', () =>
  * {@link https://webpack.js.org/configuration/}
  * 
  */
-gulp.task('scripts', callback => {
+function scripts(callback) {
     webpack(require('./webpack.config.js'), function(err, stats) {
     	if (err) {
         	console.log(err.toString());
     	}
-      	console.log(stats.toString());
-      	callback();
+        console.log(stats.toString());
+        callback();
     });
+}; 
+
+/**
+ * Specify gulp tasks
+ * 
+ * 
+ */
+
+gulp.task('default', () => {
+
+  gulp.watch('./src/scss/*/*.scss', styles);
+  gulp.watch('./src/scss/*.scss', styles);
+  gulp.watch('./src/js/*/*.js', scripts);
+  gulp.watch('./src/js/*.js', scripts);
+
 });
 
-
-/**
- * Debug gulp task
- * 
- * Compiles the SCSS files to CSS but doesn't minify or autoprefix it, for debugging. 
- * 
- * For options check out:
- * {@link https://github.com/postcss/autoprefixer#options}
- * 
- */
-gulp.task('debug', () => 
-    gulp.src('./src/scss/style.scss')
-    .pipe(sass({}).on('error', sass.logError))
-    .pipe(gulp.dest('./dist/'))
-);
-
-/**
- * Watch gulp task
- * 
- * Watch for all the SCSS and JS files and execute 'styles' 
- * and 'scripts' tasks when a change is made in the files in the paths.
- * 
- */
-gulp.task('watch', () => {
-	gulp.watch('./src/scss/*/*.scss', ['styles']);
-	gulp.watch('./src/js/*/*.js', ['scripts']);
-	gulp.watch('./src/js/*.js', ['scripts']);
-});
-
-/**
- * Default gulp task
- * 
- * Run this by typing the following in the command line:
- * 
- * gulp
- * 
- * This will initiate gulp, perform the default task and continue watching
- * all of the files that are being watched.
- * 
- */
-gulp.task('default', ['watch']);
+gulp.task('styles', styles);
+gulp.task('scripts', scripts);
